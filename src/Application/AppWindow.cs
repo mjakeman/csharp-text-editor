725ee80fc3da8d8753103148e7810269ee50e4cc
Matthew Jakeman <mjak923@aucklanduni.ac.nz>
2021-05-16 01:57:11 +1200

    Rough implementation of Insert() operation for piece table

--- src/Application/AppWindow.cs
+++ src/Application/AppWindow.cs
@@ -11,6 +11,10 @@ namespace Bluetype.Application
 
         private Box contentBox;
         private Entry insertEntry;
+        private Label label;
+        private SpinButton spinButton;
+
+        public Document Document { get; set; }
 
         public AppWindow()
         {
@@ -27,15 +31,21 @@ namespace Bluetype.Application
             contentBox = Box.New(Orientation.Vertical, 0);
             this.Child = contentBox;
 
-            var label = Label.New(string.Empty);
+            label = Label.New(string.Empty);
             label.SetSingleLineMode(false);
             contentBox.PackStart(label, true, true, 0);
 
             var hbox = Box.New(Orientation.Horizontal, 0);
             contentBox.PackEnd(hbox, false, false, 0);
 
+            // Text to insert
             insertEntry = Entry.New();
             hbox.PackStart(insertEntry, true, true, 0);
+
+            // Index to insert
+            spinButton = SpinButton.NewWithRange(0, Int32.MaxValue, 1);
+            spinButton.SetDigits(0);
+            hbox.PackStart(spinButton, false, false, 0);
 
             var button = new Button("Insert");
             button.Label = "Insert"; // <-- TODO: While construct args are broken
@@ -43,8 +53,8 @@ namespace Bluetype.Application
             hbox.PackEnd(button, false, false, 0);
 
             // Start by creating a document
-            var document = Document.NewFromString("The quick brown fox jumps over the lazy dog.");
-            label.LabelProp = document.GetContents();
+            Document = Document.NewFromString("The quick brown fox jumps over the lazy dog.");
+            label.LabelProp = Document.GetContents();
 
             // Set visible
             headerBar.ShowAll();
@@ -53,7 +63,12 @@ namespace Bluetype.Application
 
         void Insert(Button button, EventArgs args)
         {
+            var index = spinButton.GetValue();
+            var text = insertEntry.GetText();
+            Document.Insert((int)index, text);
 
+            // Update label
+            label.SetText(Document.GetContents());
         }
     }
 }
--- src/Document/Document.cs
+++ src/Document/Document.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
@@ -15,12 +16,33 @@ namespace Bluetype.Document
         // This paper demonstrates the piece-table based data structure used to store the
         // document's text and subsequent modifications.
 
+        // Also called: Piece, Span, etc
         public record Descriptor(bool addBuffer, int offset, int length);
 
         private LinkedList<Descriptor> _pieceTable = new();
 
         private readonly string _file;
         private string _add;
+
+        private (LinkedListNode<Descriptor> desc, int baseIndex) FromIndex(int index)
+        {
+            if (index < 0)
+                throw new ArgumentOutOfRangeException();
+
+            var curIndex = 0;
+
+            for (var desc = _pieceTable.First; desc != null; desc = desc.Next)
+            {
+                curIndex += desc.Value.length;
+
+                if (curIndex > index)
+                    return (desc, curIndex - desc.Value.length);
+            }
+
+            // We might be the final element -> in which case, create a new descriptor?
+
+            return (null, -1); // throw new IndexOutOfRangeException();
+        }
 
         public string GetContents()
         {
@@ -35,6 +57,69 @@ namespace Bluetype.Document
             }
 
             return builder.ToString();
+        }
+
+        public void Insert(int index, string text)
+        {
+            if (text.Length == 0)
+                return;
+
+            if (index < 0)
+                throw new IndexOutOfRangeException();
+
+            // TODO: Use a better buffer data structure?
+            var newOffset = _add.Length;
+            var newLength = text.Length;
+            
+            var newDesc = new Descriptor(true, newOffset, newLength);
+            _add += text;
+
+            var (existingPieceNode, baseIndex) = FromIndex(index);
+
+            if (existingPieceNode == null)
+            {
+                // Node is null - assume we're appending to the end
+                _pieceTable.AddLast(newDesc);
+                return;
+            }
+
+            Descriptor existingDesc = existingPieceNode.Value;
+
+            // If we are at a piece boundary, we can simply
+            // insert at the beginning and return.
+            if (index == existingDesc.offset)
+            {
+                _pieceTable.AddBefore(existingPieceNode, newDesc);
+                return;
+            }
+
+            // We split the above piece into three, for the
+            // first part, the newly inserted part, and the
+            // remaning last part.
+
+            // Find the point within the piece in which to split
+            var insertionOffset = index - baseIndex;
+
+            // Calculate new size/offset for start and end pieces
+            var startLength = insertionOffset;
+            var endLength = existingDesc.length - startLength;
+            var endOffset = startLength;
+
+            if (endLength != 0)
+            {
+                var endDesc = new Descriptor(existingDesc.addBuffer, endOffset, endLength);
+                _pieceTable.AddAfter(existingPieceNode, endDesc);
+            }
+                
+            _pieceTable.AddAfter(existingPieceNode, newDesc);
+
+            // Finally: resize the existing piece to go up until the
+            // point of insertion, or alternatively, remove the piece if
+            // length is zero.
+            if (insertionOffset != 0)
+                existingPieceNode.Value = existingDesc with { length = startLength };
+            else
+                _pieceTable.Remove(existingDesc);
         }
 
         private Document(string data)
