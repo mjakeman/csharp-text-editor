1fc9138a8643e62130b370f6f9c2921019373295
Matthew Jakeman <mjak923@aucklanduni.ac.nz>
2021-05-24 20:45:07 +1200

    Switch to cairo for rendering, use IMContext for Input

--- src/Application/DocumentView.cs
+++ src/Application/DocumentView.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using Gtk;
 
 using cairo;
+using Gdk;
 using Pango;
 
 namespace Bluetype.Application
@@ -17,17 +18,33 @@ namespace Bluetype.Application
 
         private string cachedText;
 
-        private Label textLayout; // <-- replace with something more sophisticated
-        // private DrawingArea area;
+        private EventBox eventBox;
+
+        private DrawingArea area;
+
+        private IMContext context;
 
         public DocumentView(Document document)
         {
-            textLayout = Label.New(string.Empty);
-            // area = DrawingArea.New();
-            // area.OnDraw += Render();
-            Child = textLayout;
-            SetDocument(document);            
+            // Setup IMContext
+            context = IMContextSimple.New();
+            context.OnCommit += OnEnterText;
+            
+            // Add event box to catch keyboard input
+            eventBox = EventBox.New();
+            eventBox.OnKeyPressEvent += OnKeydown;
+            eventBox.CanFocus = true;
+            eventBox.OnRealize += (o, e) => context.SetClientWindow(eventBox.GetWindow());
+            Child = eventBox;
 
+            // Drawing area to display rich text
+            area = DrawingArea.New();
+            area.OnDraw += Render;
+            eventBox.Child = area;
+
+            // Load data
+            SetDocument(document);
+            
             ShowAll();
         }
 
@@ -64,6 +81,18 @@ namespace Bluetype.Application
             Redraw();
         }
 
+        private void OnKeydown(object sender, KeyPressEventSignalArgs e)
+        {
+            Console.WriteLine("Keydown " + e.@event.Keyval);
+            context.FilterKeypress(e.@event); // Check result?
+        }
+
+        private void OnEnterText(object sender, IMContext.CommitSignalArgs e)
+        {
+            doc.Insert(cursorIndex, e.Str);
+            cursorIndex += e.Str.Length;
+        }
+
         private void UpdateCache()
         {
             var builder = new StringBuilder();
@@ -77,17 +106,35 @@ namespace Bluetype.Application
 
         private void Redraw()
         {
-            textLayout.LabelProp = cachedText.Insert(cursorIndex, "|");
-            // area.QueueDraw();
+            // textLayout.LabelProp = cachedText.Insert(cursorIndex, "|");
+            area.QueueDraw();
         }
 
-        // private void Render(object sender, DrawingArea.DrawSignalArgs e)
-        // {
-        //     Debug.Assert(area == sender);
+        private void Render(object sender, DrawingArea.DrawSignalArgs e)
+        {
+            Debug.Assert(area == sender);
 
-        //     Context cr = e.Cr;
+            int xDist = 30;
+            int yDist = 50;
+
+            cairo.Context cr = e.Cr;
+            cr.SetFontSize(16);
             
+            cr.TextExtents(cachedText, out TextExtents lineExtents);
+            var height = lineExtents.height;
             
-        // }
+            cr.TextExtents(cachedText.Substring(0, cursorIndex), out TextExtents cursorExtents);
+            var xPos = cursorExtents.width;
+
+            cr.FontExtents(out FontExtents fontExtents);
+            
+            cr.SetSourceRgba(1.0, 0, 0, 1.0);
+            cr.Rectangle(xDist + cursorExtents.xAdvance - 0.5, yDist + fontExtents.Descent, 1, -(fontExtents.Descent + fontExtents.Ascent));
+            cr.Fill();
+            
+            cr.MoveTo(xDist, yDist);
+            cr.SetSourceRgba(0, 0, 0, 1.0);
+            cr.ShowText(cachedText);
+        }
     }
 }
